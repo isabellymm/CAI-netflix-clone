fd98abd42f58c2bd5c4719030939dec208f579f9
isabelly Martins Mota <isabellymartinsmota6@gmail.com>
2024-03-15 10:25:01 -0300

    integração api

---
+++ src/Home.js
@@ -0,0 +1,57 @@
+//Componente Inicial
+import { useEffect } from 'react';
+import './Home.css';
+import React, { useState } from 'react';
+
+export default function Home() {
+    const [tvs, setTVs] = useState()
+
+    let getData = async () => {
+        let API_KEY = '76bd8a2f7dd676fbf2a87971267b91e8'
+        let URI = 'https://api.themoviedb.org/3/genre/tv/list?api_key='+API_KEY
+
+        let data = await fetch(URI)
+
+        let result = data.json()
+
+        return result
+    }
+
+    useEffect(()=>{
+
+        getData().then((data)=>{
+            console.log(data)
+            setTVs(data)
+        })
+
+    },[])
+
+    let showTvs = () => {
+        let html = []
+
+        tvs?.genres.forEach(element => {
+            html.push(<p key={element.id}> 
+                        <p> <label>Id:</label> {element.id}</p>
+                        <p> <label>Name:</label> {element.name}</p>
+                      </p>
+                     )
+        });
+
+        return html
+    }
+
+    return (
+        <div>
+
+            {/*Navegar*/}
+            {/*Conteudo*/}
+            {/*Rodapé*/}
+
+            <h1>Teste</h1>
+
+            {showTvs()}
+
+
+        </div>
+    )
+}
